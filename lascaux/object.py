9f02d35d563c954f3daf775ff5b1bfab2859e7da
Derek McTavish Mounce <d.mounce@silkseed.com>
2010-08-17 20:23:44 -0400

    Updated to use UpperCamelCase for arguments.

--- lascaux/object.py
+++ lascaux/object.py
@@ -11,44 +11,45 @@ class Object:
     __lib_path__ = None
     __self_path__ = None
 
-    def get_exec_path(self, force=False):
+    def get_exec_path(self, Force=False):
         """
         Absolute path of os.getcwd().
         """
-        if not self.__exec_path__ or force:
+        if not self.__exec_path__ or Force:
             self.__exec_path__ = os.path.abspath(os.getcwd())
         return self.__exec_path__
 
-    def get_lib_path(self, force=False):
+    def get_lib_path(self, Force=False):
         """
         Points to the absolute path of the lascaux library
         """
-        if not self.__lib_path__ or force:
+        if not self.__lib_path__ or Force:
             self.__lib_path__ = os.path.abspath(
                 os.path.dirname(lascaux.__file__))
         return self.__lib_path__
 
-    def get_self_path(self, force=False, source=True):
+    def get_self_path(self, Force=False, Source=True):
         """
         Absolute path for the module of the current class
         (applies to subclasses as well)
+        Source==True will return the py, not the pyc or pyo
         """
-        if not self.__self_path__ or force:
+        if not self.__self_path__ or Force:
             if self.__module__ == "__main__":
                 self.__self_path__ = os.path.join(os.getcwd(), sys.argv[0])
             else:
                 self.__self_path__ = sys.modules[self.__module__].__file__
-        if source:
+        if Source:
             if os.path.splitext(self.__self_path__)[1] in (".pyc", ".pyo"):
                 return os.path.splitext(self.__self_path__)[0] + ".py"
         return self.__self_path__
 
-    def determine_dot_path(self, file):
+    def determine_dot_path(self, File):
         """
         Returns the dot path for importing the module
         contained in a given file.
         """
-        file = os.path.splitext(os.path.abspath(file))[0]
+        file = os.path.splitext(os.path.abspath(File))[0]
         for path in sys.path:
             path = os.path.abspath(path)
             if file.startswith(path):

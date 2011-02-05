94279a073a92b8e3e4beb79a7585fb6d3876dc99
Derek McTavish Mounce <d.mounce@silkseed.com>
2011-02-05 14:25:05 -0500

    Added back logging, removed print.

--- lascaux_app/plugins/hello_world/index.py
+++ lascaux_app/plugins/hello_world/index.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from lascaux import Controller
-from lascaux import config
 
 
 class Index(Controller):
     
     def get(self, p, place=None):
-        print config
         self.render('index', message=u"Hello World")
         self.final('index', app_package=True)
